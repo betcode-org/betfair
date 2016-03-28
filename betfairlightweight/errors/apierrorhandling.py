580b9117d24e608f3f14273a58d84362e2ecc9a0
LiamPauling <paulingliam@gmail.com>
2016-03-28 18:20:27 +0100

    error tidy up apimethod change

--- betfairlightweight/errors/apierrorhandling.py
+++ betfairlightweight/errors/apierrorhandling.py
@@ -32,10 +32,10 @@ def api_order_error_handling(response, params=None, method=None):
         response_error_code = response['result']['errorCode']
         description = apierrors.EXECUTION_REPORT_ERROR_CODE[response_error_code]
         logging.warning('API Execution %s: %s' % (response['result']['status'], description))
+        logging.warning('Bug error %s, request: %s' % (method, params))
+        logging.warning('Bug error %s, response: %s' % (method, response))
         for order in response['result']['instructionReports']:
             if order['status'] != 'SUCCESS':
-                logging.warning('Bug error %s, request: %s' % (method, params))
-                logging.warning('Bug error %s, response: %s' % (method, response))
                 error_code = order.get('errorCode')
                 description = apierrors.INSTRUCTION_REPORT_ERROR_CODE.get(error_code)
                 logging.warning(' Instruction %s: %s' % (error_code, description))
