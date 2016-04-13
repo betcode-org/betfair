f6c078681beebfddd0c79ec0162fe5780b2904a2
LiamPauling <paulingliam@gmail.com>
2016-04-13 20:25:58 +0100

    travis added setup modified to include tests

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
-from betfairlightweight.__init__ import __version__
+import sys
 from distutils.core import setup, Extension
+from setuptools.command.test import test as TestCommand
+from betfairlightweight.__init__ import __version__
+
+
+TEST_REQUIRES = ['pytest']
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--verbose']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
 
 
 setup(
@@ -12,5 +30,8 @@ setup(
         license='',
         author='liampauling',
         author_email='',
-        description='Lightweight python wrapper for Betfair API-NG'
+        description='Lightweight python wrapper for Betfair API-NG',
+        test_suite='tests',
+        tests_require=TEST_REQUIRES,
+        cmdclass={'test': PyTest}
 )
