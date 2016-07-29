40b5162cafd2a7b3486e7e9d272d0cdf44e1a13f
LiamPauling <paulingliam@gmail.com>
2016-07-29 07:25:54 +0100

    betting complete with tests up to marketcatalogue need to add runners strip date?

--- betfairlightweight/endpoints/betting.py
+++ betfairlightweight/endpoints/betting.py
@@ -1,7 +1,7 @@
 import datetime
 
 from .baseendpoint import BaseEndpoint
-from ..resources.resources import EventTypes
+from .. import resources
 
 
 class Betting(BaseEndpoint):
@@ -12,4 +12,46 @@ class Betting(BaseEndpoint):
         date_time_sent = datetime.datetime.now()
         method = '%s%s' % (self.URI, 'listEventTypes')
         response = self.request(method, params)
-        return self.process_response(response.json(), EventTypes, date_time_sent)
+        return self.process_response(response.json(), resources.EventTypeResult, date_time_sent)
+
+    def list_competitions(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listCompetitions')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.CompetitionResult, date_time_sent)
+
+    def list_time_ranges(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listTimeRanges')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.TimeRangeResult, date_time_sent)
+
+    def list_events(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listEvents')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.EventResult, date_time_sent)
+
+    def list_market_types(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listMarketTypes')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.MarketTypeResult, date_time_sent)
+
+    def list_countries(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listCountries')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.CountryResult, date_time_sent)
+
+    def list_venues(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listVenues')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.VenueResult, date_time_sent)
+
+    def list_market_catalogue(self, params=None):
+        date_time_sent = datetime.datetime.now()
+        method = '%s%s' % (self.URI, 'listMarketCatalogue')
+        response = self.request(method, params)
+        return self.process_response(response.json(), resources.MarketCatalogue, date_time_sent)
