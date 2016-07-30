c9f5d883a0c42fd1651b71790f3012db937d803b
LiamPauling <paulingliam@gmail.com>
2016-07-30 14:51:59 +0100

    strip datetime added datetime attributes added dict attributes added tests updated / added

--- tests/test_baseresource.py
+++ tests/test_baseresource.py
@@ -13,8 +13,9 @@ class BaseResourceInit(unittest.TestCase):
         assert base_resource.Meta.identifier == 'id'
         assert base_resource.Meta.attributes == {'id': 'id'}
         assert base_resource.Meta.sub_resources == {}
-        assert base_resource.date_time_sent is None
-        assert base_resource.date_time_created is not None
+        assert base_resource.Meta.datetime_attributes == ()
+        assert base_resource.datetime_sent is None
+        assert base_resource.datetime_created is not None
         assert base_resource.elapsed_time is None
         assert base_resource.id is None
         with self.assertRaises(AttributeError):
@@ -25,7 +26,7 @@ class BaseResourceInit(unittest.TestCase):
         base_resource = BaseResource(date_time_sent=datetime.datetime(2003, 8, 4, 12, 30, 45),
                                      **mock_response.json())
 
-        assert base_resource.date_time_sent == datetime.datetime(2003, 8, 4, 12, 30, 45)
+        assert base_resource.datetime_sent == datetime.datetime(2003, 8, 4, 12, 30, 45)
         assert base_resource.id == 12345
         assert base_resource.elapsed_time > 0
 
@@ -33,7 +34,7 @@ class BaseResourceInit(unittest.TestCase):
         mock_response = create_mock_json('tests/resources/base_resource_sub.json')
 
         class Model(BaseResource):
-            class Meta:
+            class Meta(BaseResource.Meta):
                 identifier = 'model'
                 attributes = {'MainId': 'main_id',
                               'SubId': 'sub_id'}
@@ -47,3 +48,69 @@ class BaseResourceInit(unittest.TestCase):
 
         with self.assertRaises(AttributeError):
             assert model_response.not_in
+
+    def test_strip_datetime(self):
+        base_resource = BaseResource()
+        for string in ['2100-06-01T10:10:00.000Z', '2100-06-01T10:10:00.00Z', '2100-06-01T10:10:00.0Z']:
+            stripped = base_resource.strip_datetime(string)
+            assert type(stripped) == datetime.datetime
+
+        stripped = base_resource.strip_datetime(None)
+        assert not stripped
+
+        stripped = base_resource.strip_datetime('45')
+        assert not stripped
+
+        integer = 1465631675000
+        stripped = base_resource.strip_datetime(integer)
+        assert type(stripped) == datetime.datetime
+
+        stripped = base_resource.strip_datetime(None)
+        assert not stripped
+
+        stripped = base_resource.strip_datetime('45')
+        assert not stripped
+
+    def test_strip_datetime_resource(self):
+        mock_response = create_mock_json('tests/resources/base_resource_sub.json')
+
+        class Model(BaseResource):
+            class Meta(BaseResource.Meta):
+                identifier = 'model'
+                attributes = {'MainId': 'main_id',
+                              'Datetime': 'datetime',
+                              'DatetimeInt': 'datetime_int',
+                              'DatetimeWrong': 'datetime_wrong'}
+                sub_resources = {'Id': BaseResource}
+                datetime_attributes = ('Datetime', 'DatetimeInt', 'DatetimeWrong')
+
+        model_response = Model(**mock_response.json())
+
+        assert model_response.main_id == 12345
+        assert model_response.id.id == 6789
+        assert model_response.datetime == datetime.datetime.strptime('2100-06-01T10:10:00.0Z', '%Y-%m-%dT%H:%M:%S.%fZ')
+        assert model_response.datetime_int == datetime.datetime.fromtimestamp(1465631675000 / 1e3)
+        assert model_response.datetime_wrong == 'wrong'
+
+    def test_data_dict(self):
+        mock_response = create_mock_json('tests/resources/base_resource_dict.json')
+
+        class Runner(BaseResource):
+            class Meta(BaseResource.Meta):
+                identifier = 'runners'
+                attributes = {'Id': 'id',
+                              'name': 'name'}
+
+        class Model(BaseResource):
+            class Meta(BaseResource.Meta):
+                identifier = 'model'
+                attributes = {'Id': 'id'}
+                sub_resources = {'Runners': Runner}
+                dict_attributes = {'Runners': 'Id'}
+
+        model_response = Model(**mock_response.json())
+
+        assert model_response.id == 12345
+        assert len(model_response.runners) == 2
+        assert model_response.runners.get(1234) is not None
+        assert model_response.runners[1234].name == 'second'
