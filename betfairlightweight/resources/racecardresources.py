306a4799318b891ef8d22ab1d996fb38a2225290
LiamPauling <paulingliam@gmail.com>
2016-09-18 13:24:38 +0100

    0.6.1 racecard bug fix extra resources added for racecard

--- betfairlightweight/resources/racecardresources.py
+++ betfairlightweight/resources/racecardresources.py
@@ -95,14 +95,87 @@ class InPlayHint(BaseResource):
         }
 
 
+class DaysSinceLastRun(BaseResource):
+    class Meta(BaseResource.Meta):
+        identifier = 'days_since_last_run'
+        attributes = {
+            'days': 'days',
+            'type': 'type'
+        }
+
+
+class Jockey(BaseResource):
+    class Meta(BaseResource.Meta):
+        identifier = 'jockey'
+        attributes = {
+            'jockeyId': 'jockey_id',
+            'name': 'name'
+        }
+
+
+class Selection(BaseResource):
+    class Meta(BaseResource.Meta):
+        identifier = 'selection'
+        attributes = {
+            'marketId': 'market_id',
+            'marketType': 'market_type',
+            'selectionId': 'selection_id'
+        }
+
+
+class Trainer(BaseResource):
+    class Meta(BaseResource.Meta):
+        identifier = 'trainer'
+        attributes = {
+            'location': 'location',
+            'name': 'name',
+            'trainerId': 'trainer_id'
+        }
+
+
+class Runner(BaseResource):
+    class Meta(BaseResource.Meta):
+        identifier = 'runner'
+        attributes = {
+            'age': 'age',
+            'comment': 'comment',
+            'gender': 'gender',
+            'horseId': 'horse_id',
+            'isNonRunner': 'is_non_runner',
+            'longHandicap': 'long_handicap',
+            'name': 'name',
+            'ownerColours': 'owner_colours',
+            'recentForm': 'recent_form',
+            'saddleCloth': 'saddle_cloth',
+            'starRating': 'star_rating',
+            'timeform123Place': 'timeform_123_place',
+            'weight': 'weight',
+            'winsAtCourse': 'wins_at_course',
+            'winsAtCourseAndDistance': 'wins_at_course_and_distance',
+            'winsAtDistance': 'wins_at_distance'
+        }
+        sub_resources = {
+            'daysSinceLastRun': DaysSinceLastRun,
+            'jockey': Jockey,
+            'selections': Selection,
+            'trainer': Trainer
+        }
+
+
 class RaceCard(BaseResource):
     class Meta(BaseResource.Meta):
         identifier = 'race_card'
         attributes = {
+            'bettingForecastText': 'betting_forecast_text',
+            'comment': 'comment',
             'minimumWeight': 'minimum_weight',
+            'numberOfNonRunners': 'number_of_non_runners',
+            'numberOfRunners': 'number_of_runners',
             'prize': 'prize',
+            'timeform123Text': 'timeform_123_text'
         }
         sub_resources = {
             'inPlayHints': InPlayHint,
             'race': Race,
+            'runners': Runner
         }
