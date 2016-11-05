5ea0bae58dd4721f9b43633afb2a50ccb1d96d4a
LiamPauling <paulingliam@gmail.com>
2016-11-05 11:44:30 +0000

    listener tests complete

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -2,6 +2,7 @@ import unittest
 from unittest import mock
 
 from betfairlightweight.streaming.listener import BaseListener, StreamListener
+from tests.tools import create_mock_json
 
 
 class BaseListenerTest(unittest.TestCase):
@@ -60,9 +61,6 @@ class StreamListenerTest(unittest.TestCase):
     def test_init(self):
         assert self.stream_listener.output_queue == self.output_queue
 
-    def test_register_stream(self):
-        pass
-
     def test_on_data(self):
         pass
 
@@ -73,14 +71,43 @@ class StreamListenerTest(unittest.TestCase):
     def test_on_status(self):
         self.stream_listener._on_status({}, 1)
 
-    def test_on_change_message(self):
-        pass
+    @mock.patch('betfairlightweight.streaming.listener.StreamListener._on_connection')
+    @mock.patch('betfairlightweight.streaming.listener.StreamListener._on_status')
+    @mock.patch('betfairlightweight.streaming.listener.StreamListener._on_change_message')
+    @mock.patch('betfairlightweight.streaming.listener.StreamListener._error_handler', return_value=False)
+    def test_on_change_message(self, mock_error_handler, mock_on_change_message, mock_on_status, mock_on_connection):
+        mock_response = create_mock_json('tests/resources/streaming_connection.json')
+        self.stream_listener.on_data(mock_response.content)
+        mock_error_handler.assert_called_with(mock_response.json(), mock_response.json().get('id'))
+        mock_on_connection.assert_called_with(mock_response.json(), mock_response.json().get('id'))
 
-    def test_add_stream(self):
-        pass
+        mock_response = create_mock_json('tests/resources/streaming_status.json')
+        self.stream_listener.on_data(mock_response.content)
+        mock_error_handler.assert_called_with(mock_response.json(), mock_response.json().get('id'))
+        mock_on_status.assert_called_with(mock_response.json(), mock_response.json().get('id'))
+
+        mock_response = create_mock_json('tests/resources/streaming_mcm_update.json')
+        self.stream_listener.on_data(mock_response.content)
+        mock_error_handler.assert_called_with(mock_response.json(), mock_response.json().get('id'))
+        mock_on_change_message.assert_called_with(mock_response.json(), mock_response.json().get('id'))
+
+    @mock.patch('betfairlightweight.streaming.listener.Stream', return_value=123)
+    def test_add_stream(self, mock_stream):
+        new_stream = self.stream_listener._add_stream(1, 'op')
+        assert new_stream == 123
+        mock_stream.assert_called_with(1, 'op', self.output_queue)
 
     def test_error_handler(self):
-        pass
+        mock_response = create_mock_json('tests/resources/streaming_connection.json')
+        self.stream_listener._error_handler(mock_response.json(), 1)
+
+        error_data = {'errorMessage': 'AppKey is not valid', 'connectionClosed': True, 'connectionId': '005-015616813698-533678', 'op': 'status', 'id': 2, 'errorCode': 'INVALID_APP_KEY', 'statusCode': 'FAILURE'}
+        return_value = self.stream_listener._error_handler(error_data, 1)
+        assert return_value is True
+
+        error_data = {'errorMessage': 'Customer tried to subscribe to more markets than allowed to', 'connectionClosed': False, 'connectionId': '005-015616813698-533678', 'op': 'status', 'id': 2, 'errorCode': 'SUBSCRIPTION_LIMIT_EXCEEDED', 'statusCode': 'FAILURE'}
+        return_value = self.stream_listener._error_handler(error_data, 1)
+        assert return_value is None
 
     def test_str(self):
         assert str(self.stream_listener) == '<StreamListener>'
