82e47843c995182ab96e4b03dbbf922f9c105a85
Rory Cole <rory.cole1990@gmail.com>
2017-01-22 20:27:29 +1100

    Move filters to methods returning dicts rather than classes requiring secondary serialise calls. Add enums to have complete coverage of enums from betfair docs. Add price levels and time functions to utils.

--- betfairlightweight/utils.py
+++ betfairlightweight/utils.py
@@ -3,6 +3,8 @@ import datetime
 from .enums import MockParams
 from .exceptions import StatusCodeError
 
+TICK_SIZES = {1.0: 0.01, 2.0: 0.02, 3.0: 0.05, 4.0: 0.1, 6.0: 0.2, 10.0: 0.5,
+              20.0: 1.0, 30.0: 2.0, 50.0: 5.0, 100.0: 10.0, 1000.0: 1000}
 
 def check_status_code(response, codes=None):
     """Checks response.status_code is in codes
@@ -90,3 +92,68 @@ def update_available(available, book_update, deletion_select):
 
         if not updated:
             available.append(book)
+
+
+def create_timerange(start, end):
+    """
+    Create an isoformat date range for betfair filtering.
+
+    :param start: start of time range.
+    :type start: datetime.datetime
+    :param end: end of time range.
+    :type end: datetime.datetime
+    :returns: isoformat date range.
+    :rtype: dict
+    """
+    try:
+        return {'from': start.isoformat(), 'to': end.isoformat()}
+    except:
+        raise ValueError('Failed to create isoformat dates.')
+
+
+def get_price_increment(price):
+    '''
+    Get the tick size given a reference price.
+
+    :param price: reference price.
+    :type price: float
+    :returns: Tick size increment.
+    :rtype: float
+    '''
+    price_list = sorted(TICK_SIZES.keys())
+    closest_price = price_list[bisect_right(price_list, price)-1]
+    return TICK_SIZES.get(closest_price)
+
+
+def get_bf_prices():
+    '''
+    Get all tradeable prices for betfair.
+
+    :returns: list of all prices tradeable on betfair.
+    :rtype: list
+    '''
+    level = 1.01
+    bet_levels = []
+    while level <= 1000.0:
+        bet_levels.append(float(str(level)))
+        if level < 1.995:
+            level += 0.01
+        elif level < 2.99:
+            level += 0.02
+        elif level < 3.99:
+            level += 0.05
+        elif level < 5.99:
+            level += 0.1
+        elif level < 9.99:
+            level += 0.2
+        elif level < 19.99:
+            level += 0.5
+        elif level < 29.99:
+            level += 1.0
+        elif level < 49.99:
+            level += 2.0
+        elif level < 99.99:
+            level += 5.0
+        elif level < 1001.0:
+            level += 10.0
+    return bet_levels
