156eeaabc83b0cb663062220b13b26e81d1c9057
Aaron Berk <aaron.george.berk@gmail.com>
2017-03-06 10:08:39 +0000

    Add NullHandler to top level package logger

--- betfairlightweight/__init__.py
+++ betfairlightweight/__init__.py
@@ -1,9 +1,21 @@
+import logging
+
 from .apiclient import APIClient
 from .exceptions import BetfairError
-from .streaming import StreamListener
 from .filters import MarketFilter, StreamingMarketFilter, StreamingMarketDataFilter
+from .streaming import StreamListener
 
 
 __title__ = 'betfairlightweight'
 __version__ = '0.9.9'
 __author__ = 'Liam Pauling'
+
+# Set default logging handler to avoid "No handler found" warnings.
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
