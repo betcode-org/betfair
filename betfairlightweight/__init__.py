fc8d4d852c94c79a642e390b3188243b74c4934a
LiamPauling <paulingliam@gmail.com>
2017-04-08 09:02:33 +0100

    version 1.0.0 All pulls merged

--- betfairlightweight/__init__.py
+++ betfairlightweight/__init__.py
@@ -6,7 +6,7 @@ from .streaming import StreamListener
 from . import filters
 
 __title__ = 'betfairlightweight'
-__version__ = '0.9.10'
+__version__ = '1.0.0'
 __author__ = 'Liam Pauling'
 
 # Set default logging handler to avoid "No handler found" warnings.
