9ccc08d3fda23ce88e9f1b97edada3298fa96860
Edwin Goddard <edwin.goddard@gmail.com>
2017-05-28 00:58:46 +0100

    Test LegacyData init

--- tests/test_accountresources.py
+++ tests/test_accountresources.py
@@ -4,6 +4,7 @@ import unittest
 import datetime
 
 from betfairlightweight import resources
+from betfairlightweight.resources.accountresources import LegacyData
 
 from tests.tools import create_mock_json
 
@@ -36,3 +37,26 @@ class AccountTest(unittest.TestCase):
         resource = resources.TransferFunds(transactionId=123)
         assert isinstance(resource, resources.TransferFunds)
         assert resource.transaction_id == 123
+
+    def test_legacy_data(self):
+        legacy_data = LegacyData('avgPrice','betCategoryType', 'betSize', 'betType', 'eventId', 
+            'eventTypeId', 'fullMarketName', 'marketName', 'grossBetAmount', 'transactionId', 'marketType',
+            '2017-07-25 12:34', 'selectionId', '2017-08-09 10:20', 'transactionType','winLose', 'selectionName', 'commissionRate')
+        assert legacy_data.avg_price == 'avgPrice'
+        assert legacy_data.bet_category_type == 'betCategoryType'
+        assert legacy_data.bet_size == 'betSize'
+        assert legacy_data.bet_type == 'betType'
+        assert legacy_data.event_id == 'eventId'
+        assert legacy_data.event_type_id == 'eventTypeId'
+        assert legacy_data.full_market_name == 'fullMarketName'
+        assert legacy_data.market_name == 'marketName'
+        assert legacy_data.gross_bet_amount == 'grossBetAmount'
+        assert legacy_data.transaction_id == 'transactionId'
+        assert legacy_data.market_type == 'marketType'
+        assert legacy_data.placed_date == datetime.datetime(2017,7,25,12,34)
+        assert legacy_data.selection_id == 'selectionId'
+        assert legacy_data.start_date == datetime.datetime(2017,8,9,10,20)
+        assert legacy_data.transaction_type == 'transactionType'
+        assert legacy_data.win_lose == 'winLose'
+        assert legacy_data.selection_name == 'selectionName'
+        assert legacy_data.commission_rate == 'commissionRate'
