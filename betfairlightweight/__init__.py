85d81ac27eef733f6cd4b03f041c6e442c40fdad
LiamPauling <paulingliam@gmail.com>
2017-08-27 11:21:40 +0100

    version bump

--- betfairlightweight/__init__.py
+++ betfairlightweight/__init__.py
@@ -6,7 +6,7 @@ from .streaming import StreamListener
 from . import filters
 
 __title__ = 'betfairlightweight'
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 __author__ = 'Liam Pauling'
 
 # Set default logging handler to avoid "No handler found" warnings.
