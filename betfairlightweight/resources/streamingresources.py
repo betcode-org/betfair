fe0f6c69524844d11b1a7e719d42d47ca43f5d12
agberk <aaron.george.berk@gmail.com>
2018-02-20 19:04:38 +0000

    Make openDate a kwarg for MarketDefinition
    Fixes liampauling/betfair#157

--- betfairlightweight/resources/streamingresources.py
+++ betfairlightweight/resources/streamingresources.py
@@ -99,8 +99,8 @@ class MarketDefinition(object):
 
     def __init__(self, betDelay, bettingType, bspMarket, bspReconciled, complete, crossMatching, discountAllowed,
                  eventId, eventTypeId, inPlay, marketBaseRate, marketTime, numberOfActiveRunners, numberOfWinners,
-                 openDate, persistenceEnabled, regulators, runnersVoidable, status, timezone, turnInPlayEnabled,
-                 version, runners, countryCode=None, eachWayDivisor=None, venue=None, settledTime=None,
+                 persistenceEnabled, regulators, runnersVoidable, status, timezone, turnInPlayEnabled,
+                 version, runners, openDate=None, countryCode=None, eachWayDivisor=None, venue=None, settledTime=None,
                  suspendTime=None, marketType=None, lineMaxUnit=None, lineMinUnit=None, lineInterval=None, name=None,
                  eventName=None, priceLadderDefinition=None, keyLineDefinition=None, raceType=None):
         self.bet_delay = betDelay
@@ -119,7 +119,7 @@ class MarketDefinition(object):
         self.market_type = marketType
         self.number_of_active_runners = numberOfActiveRunners
         self.number_of_winners = numberOfWinners
-        self.open_date = BaseResource.strip_datetime(openDate)
+        self.open_date = BaseResource.strip_datetime(openDate) if openDate else None
         self.persistence_enabled = persistenceEnabled
         self.regulators = regulators
         self.runners_voidable = runnersVoidable
