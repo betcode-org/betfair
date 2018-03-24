a312c8c0ef56a843cd0831c9521fa7693cc29264
LiamPauling <paulingliam@gmail.com>
2018-03-24 07:05:24 +0000

    version change for pypi

--- betfairlightweight/__init__.py
+++ betfairlightweight/__init__.py
@@ -6,7 +6,7 @@ from .streaming import StreamListener
 from . import filters
 
 __title__ = 'betfairlightweight'
-__version__ = '1.6.0'
+__version__ = '0.0.0b'
 __author__ = 'Liam Pauling'
 
 # Set default logging handler to avoid "No handler found" warnings.
