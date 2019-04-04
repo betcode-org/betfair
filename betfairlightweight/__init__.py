7c90b4e2587538a5e4026d1c3947decdcf18262c
Unknown <eu_genio@live.it>
2019-04-04 11:09:14 +0200

    version bump and history update

--- betfairlightweight/__init__.py
+++ betfairlightweight/__init__.py
@@ -6,7 +6,7 @@ from .streaming import StreamListener
 from . import filters
 
 __title__ = 'betfairlightweight'
-__version__ = '1.9.0'
+__version__ = '1.9.1'
 __author__ = 'Liam Pauling'
 
 # Set default logging handler to avoid "No handler found" warnings.
