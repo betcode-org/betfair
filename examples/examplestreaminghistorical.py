01728ba9a3c929334b649554039861fe27f9b8fc
LiamPauling <paulingliam@gmail.com>
2020-04-23 08:58:22 +0100

    historical streaming example updated to use generator

--- examples/examplestreaminghistorical.py
+++ examples/examplestreaminghistorical.py
@@ -2,7 +2,6 @@ import logging
 
 import betfairlightweight
 from betfairlightweight import StreamListener
-from betfairlightweight.streaming.stream import MarketStream
 
 """
 Data needs to be downloaded from:
@@ -15,57 +14,49 @@ logging.basicConfig(level=logging.INFO)
 # create trading instance (don't need username/password)
 trading = betfairlightweight.APIClient("username", "password")
 
-
-class HistoricalStream(MarketStream):
-    # create custom listener and stream
-
-    def __init__(self, listener):
-        super(HistoricalStream, self).__init__(listener)
-        with open("output.txt", "w") as output:
-            output.write("Time,MarketId,Status,Inplay,SelectionId,LastPriceTraded\n")
-
-    def on_process(self, market_books):
-        with open("output.txt", "a") as output:
-            for market_book in market_books:
-                for runner in market_book.runners:
-
-                    # how to get runner details from the market definition
-                    market_def = market_book.market_definition
-                    runners_dict = {
-                        (runner.selection_id, runner.handicap): runner
-                        for runner in market_def.runners
-                    }
-                    runner_def = runners_dict.get(
-                        (runner.selection_id, runner.handicap)
-                    )
-
-                    output.write(
-                        "%s,%s,%s,%s,%s,%s\n"
-                        % (
-                            market_book.publish_time,
-                            market_book.market_id,
-                            market_book.status,
-                            market_book.inplay,
-                            runner.selection_id,
-                            runner.last_price_traded or "",
-                        )
-                    )
-
-
-class HistoricalListener(StreamListener):
-    def _add_stream(self, unique_id, stream_type):
-        if stream_type == "marketSubscription":
-            return HistoricalStream(self)
-
-
 # create listener
-listener = HistoricalListener(max_latency=None)
+listener = StreamListener(max_latency=None)
 
-# create historical stream, update directory to file location
-stream = trading.streaming.create_historical_stream(
-    directory="/Users/liampauling/Downloads/Sites 3/xdw/api/c0a022d4-3460-41f1-af12-a0b68b136898/BASIC-1.132153978",
+# create historical stream (update directory to your file location)
+stream = trading.streaming.create_historical_generator_stream(
+    directory="/tmp/BASIC-1.132153978",
     listener=listener,
 )
 
-# start stream
-stream.start()
+# create generator
+gen = stream.get_generator()
+
+# print marketBooks
+for market_books in gen():
+    for market_book in market_books:
+        print(market_book)
+
+# record prices to a file
+with open("output.txt", "w") as output:
+    output.write("Time,MarketId,Status,Inplay,SelectionId,LastPriceTraded\n")
+
+for market_books in gen():
+    for market_book in market_books:
+        with open("output.txt", "a") as output:
+            for runner in market_book.runners:
+                # how to get runner details from the market definition
+                market_def = market_book.market_definition
+                runners_dict = {
+                    (runner.selection_id, runner.handicap): runner
+                    for runner in market_def.runners
+                }
+                runner_def = runners_dict.get(
+                    (runner.selection_id, runner.handicap)
+                )
+
+                output.write(
+                    "%s,%s,%s,%s,%s,%s\n"
+                    % (
+                        market_book.publish_time,
+                        market_book.market_id,
+                        market_book.status,
+                        market_book.inplay,
+                        runner.selection_id,
+                        runner.last_price_traded or "",
+                    )
+                )
